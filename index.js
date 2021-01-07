933e8e7787d01c729a3609f9c606afa501ea93db
AMROAMRO404 <amro.amro1999@gmail.com>
2021-01-07 15:48:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+ function weatherBalloon() {
+     let key = "cad86314552b94deb5b82fa8e5e1e33e";
+     let cityName = document.getElementById("city").value
+     if (cityName) {
+         fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}&appid=${key}`)
+             .then(response => response.json())
+             .then(data => {
+                 drawWeather(data);
+                 console.log(data)
+             }).catch(() => document.getElementById('toEnter').innerHTML = 'please enter the city name correct')
+     } else {
+         document.getElementById('toEnter').innerHTML = 'please enter the city name'
+     }
+ }
+
+ function drawWeather(d) {
+     document.getElementById('table-body').innerHTML = ""
+     for (i = 0; i < d.list.length; i++) {
+         let celciusDay = Math.round(parseFloat(d.list[i].temp.day) - 273.15);
+         let fahrenheitDar = Math.round(((parseFloat(d.list[i].temp.day) - 273.15) * 1.8) + 32);
+         let celciusMin = Math.round(parseFloat(d.list[i].temp.min) - 273.15);
+         let fahrenheitMin = Math.round(((parseFloat(d.list[i].temp.min) - 273.15) * 1.8) + 32);
+         let celciusMax = Math.round(parseFloat(d.list[i].temp.max) - 273.15);
+         let fahrenheitMax = Math.round(((parseFloat(d.list[i].temp.max) - 273.15) * 1.8) + 32);
+         document.getElementById('table-body').innerHTML +=
+             `
+                <tr>
+                    <td id="description">${d.list[i].weather[0].description}</td>
+                    <td id="temp-c">${celciusDay}</td>
+                    <td id="temp-f">${fahrenheitDar}</td>
+                    <td id="min-temp-c">${celciusMin}</td>
+                    <td id="min-temp-f">${fahrenheitMin}</td>
+                    <td id="max-temp-c">${celciusMax}</td>
+                    <td id="max-temp-f">${fahrenheitMax}</td>
+                </tr>
+             `;
+     }
+ }
